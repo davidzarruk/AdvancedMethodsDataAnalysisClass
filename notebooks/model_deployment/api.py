87dbebaf8cc0f3b73b197de304925bb680a3333c
David Zarruk <davidzarru@rappi.com>
2021-07-19 19:26:08 -0500

    update

--- notebooks/model_deployment/api.py
+++ notebooks/model_deployment/api.py
@@ -41,4 +41,4 @@ class PhishingApi(Resource):
     
     
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=80)
+    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=8888)
