7cbdf579d0ad20b6926ec3650564fbb46a742d59
David Zarruk <davidzarru@rappi.com>
2021-07-19 19:49:32 -0500

    update

--- notebooks/model_deployment/api.py
+++ notebooks/model_deployment/api.py
@@ -1,44 +1,19 @@
 #!/usr/bin/python
-from flask import Flask
-from flask_restplus import Api, Resource, fields
+from flask import Flask, request
+from flask_restful import Resource, Api
 from m09_model_deployment import predict_proba
 
 app = Flask(__name__)
+api = Api(app)
 
-api = Api(
-    app, 
-    version='1.0', 
-    title='Phishing Prediction API',
-    description='Phishing Prediction API')
-
-ns = api.namespace('predict', 
-     description='Phishing Classifier')
-   
-parser = api.parser()
-
-parser.add_argument(
-    'URL', 
-    type=str, 
-    required=True, 
-    help='URL to be analyzed', 
-    location='args')
-
-resource_fields = api.model('Resource', {
-    'result': fields.String,
-})
-
-@ns.route('/')
-class PhishingApi(Resource):
-
-    @api.doc(parser=parser)
-    @api.marshal_with(resource_fields)
+class URLpredict(Resource):
     def get(self):
-        args = parser.parse_args()
-        
         return {
-         "result": predict_proba(args['URL'])
+         "result": predict_proba(request['URL'])
         }, 200
-    
-    
+
+api.add_resource(URLpredict, '/predict')
+
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=8888)
+    app.run(debug=True)
+
