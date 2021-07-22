a109a6cd1399058dee50d2e4fc835a56c1959954
David Zarruk <davidzarru@rappi.com>
2021-07-22 09:54:15 -0500

    update

--- notebooks/model_deployment/api.py
+++ notebooks/model_deployment/api.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python
 from flask import Flask, request
-from flask_restful import Resource, Api
 from m09_model_deployment import predict_proba
 
 app = Flask(__name__)
-api = Api(app)
 
-class URLpredict(Resource):
-    def get(self):
-        return {
+@app.route('/predict', methods=['GET'])
+def URLpredict():
+    return {
          "result": predict_proba(request.args.get('URL'))
         }, 200
-
-api.add_resource(URLpredict, '/predict')
 
 @app.route('/health', methods=['GET'])
 def health():
